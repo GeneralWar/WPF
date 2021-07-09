ebf9ab6853d248ad5016db13220119bd4328fc2c
General <generalwar@outlook.com>
2021-07-09 17:47:54 +0800

    General: add extension TreeViewItem.GetTreeViewOwner, commit current editing item before edit another item

---
+++ Extensions/Extension.tree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace General.WPF
+{
+    static public partial class Extension
+    {
+        public static TreeView GetTreeViewOwner(this TreeViewItem item)
+        {
+            if (item.Parent is TreeView)
+            {
+                return item.Parent as TreeView;
+            }
+            return (item.Parent as TreeViewItem)?.GetTreeViewOwner();
+        }
+    }
+}
