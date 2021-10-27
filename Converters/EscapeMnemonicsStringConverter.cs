024229d56a9c47c0a0044d0f7516ce024f60b091
General <generalwar@outlook.com>
2021-10-27 21:21:18 +0800

    General: optimize EditableLabel in mnemonics

---
+++ Converters/EscapeMnemonicsStringConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace General.WPF
+{
+    public class EscapeMnemonicsStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string content = value as string ?? "";
+            return content != null ? content.Replace("_", "__") : value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string content = value as string ?? "";
+            return content != null ? content.Replace("__", "_") : value;
+        }
+    }
+}
